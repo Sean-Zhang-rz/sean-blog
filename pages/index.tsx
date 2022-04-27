34746cf99a69952bf7a3aedfe36d2a1ae2b15805
ProjectSean <seanrz0066@gmail.com>
2022-04-27 16:08:22 +0800

    SSR

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,67 +1,32 @@
-import Image from 'next/image'
-import Link from 'next/link'
-// import png from '../assets/images/leaf.png'
+import { GetServerSideProps, NextPage } from 'next'
+import {UAParser} from 'ua-parser-js'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+interface Props{
+  browser: {
+    name: string
+    version: string
+    major: string
+  }
+}
+const Index: NextPage<Props> = (props) => {
   return (
-    <div className={styles.container}>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}> 
-          第一篇文章
-          <Link href="/posts"><a>点击这里</a></Link>
-        </h1>
-        {/* <img src={png.src}/> */}
-
-        {/* <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div> */}
-      </main>
-
-      {/* <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer> */}
+    <div>
+      <h1 className={styles.container}>
+        你的浏览器是 {props.browser.name}
+      </h1>
     </div>
   )
+}
+
+export default Index;
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const ua = context.req.headers['user-agent'];
+  const result = new UAParser(ua).getResult();
+  return {
+    props: {
+      browser: result.browser
+    }
+  }
 }
