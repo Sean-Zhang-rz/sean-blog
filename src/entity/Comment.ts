d29c04d62d32a34fcaad81816727b0e2586e42d1
sean <770899447@qq.com>
2022-04-30 15:31:40 +0800

    创建三个实体

---
+++ src/entity/Comment.ts
@@ -0,0 +1,31 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { Post } from './Post';
+import { User } from './User';
+
+@Entity()
+export class Comment {
+  @PrimaryGeneratedColumn('increment')
+  id: number;
+
+  @Column('text')
+  content: string;
+
+  @CreateDateColumn('time')
+  createdAt: Date;
+
+  @UpdateDateColumn('time')
+  updatedAt: Date;
+
+  @ManyToOne((type) => User, (user) => user.comments)
+  user: User;
+
+  @ManyToOne((type) => Post, (post) => post.comments)
+  posts: Post;
+}
