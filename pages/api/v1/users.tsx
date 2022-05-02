8d9652aee6c0c20995fb0c7ef285d82f489736dd
sean <770899447@qq.com>
2022-05-02 20:17:47 +0800

    unique migration

--- pages/api/v1/users.tsx
+++ pages/api/v1/users.tsx
@@ -7,36 +7,28 @@ const Posts: NextApiHandler = async (req, res) => {
   console.log(req.body);
   const { username, password, passwordConfirmation } = req.body;
   res.setHeader('Content-Type', 'application/json; charset=utf-8');
-  const errors = {
-    username: [] as string[],
-    password: [] as string[],
-    passwordConfirmation: [] as string[],
-  };
+  // const errors = {
+  //   username: [] as string[],
+  //   password: [] as string[],
+  //   passwordConfirmation: [] as string[],
+  // };
 
-  if (username.trim() === '') errors.username.push('不能为空');
-  if (!/[a-zA-Z0-9]/.test(username.trim())) errors.username.push('用户名格式不合法');
-  if (username.trim().length > 42) errors.username.push('用户名太长了');
-  if (username.trim().length < 4) errors.username.push('用户名太短了');
-  if (password === '') errors.password.push('密码不能为空');
-  if (password !== passwordConfirmation) errors.passwordConfirmation.push('密码不匹配');
-  const hasError = Object.values(errors).find((v) => v.length > 0);
-  if (hasError) {
+  const connection = await getDatabaseConnection();
+  const user = new User();
+  user.username = username.trim();
+  user.password = password;
+  user.passwordConfirmation = passwordConfirmation;
+  user.passwordDigest = md5(password);
+  await user.validate();
+
+  if (user.hasErrors()) {
     res.statusCode = 422;
-    res.write(JSON.stringify(errors));
+    res.write(JSON.stringify(user.errors));
   } else {
-    const connection = await getDatabaseConnection();
-    const user = new User();
-    user.username = username.trim();
-    user.passwordDigest = md5(password);
     await connection.manager.save(user);
     res.statusCode = 200;
     res.write(JSON.stringify(user));
   }
-  res.end();
-  // Object.assign(user, req.body);
-
-  // res.statusCode = 200;
-  // res.setHeader('Content-Type', 'application/json');
   res.end();
 };
 
--- src/entity/User.ts
+++ src/entity/User.ts
@@ -8,6 +8,7 @@ import {
 } from 'typeorm';
 import { Post } from './Post';
 import { Comment } from './Comment';
+import { getDatabaseConnection } from 'lib/getDatabaseConnection';
 
 @Entity('users')
 export class User {
@@ -31,4 +32,33 @@ export class User {
 
   @OneToMany((type) => Comment, (comment) => comment.user)
   comments: Comment[];
+
+  errors = {
+    username: [] as string[],
+    password: [] as string[],
+    passwordConfirmation: [] as string[],
+  };
+  password: string;
+  passwordConfirmation: string;
+  async validate() {
+    if (this.username.trim() === '') this.errors.username.push('不能为空');
+    if (!/[a-zA-Z0-9]/.test(this.username.trim())) this.errors.username.push('用户名格式不合法');
+    if (this.username.trim().length > 42) this.errors.username.push('用户名太长了');
+    if (this.username.trim().length < 4) this.errors.username.push('用户名太短了');
+    // todo found的格式是什么？？
+    const found = await (
+      await getDatabaseConnection()
+    ).manager.find(User, {
+      username: this.username,
+    });
+    if (found.length > 0) this.errors.username.push('用户名不能重复');
+
+    if (this.password === '') this.errors.password.push('密码不能为空');
+    if (this.password !== this.passwordConfirmation)
+      this.errors.passwordConfirmation.push('密码不匹配');
+  }
+
+  hasErrors() {
+    return !!Object.values(this.errors).find((v) => v.length > 0);
+  }
 }
