8fc1d2e4de71c6f47e0bd2bc462199299241ee17
sean <770899447@qq.com>
2022-05-02 09:01:13 +0800

    渲染post

--- lib/getDatabaseConnection.tsx
+++ lib/getDatabaseConnection.tsx
@@ -1,12 +1,25 @@
 import { createConnection, getConnectionManager } from 'typeorm';
+import 'reflect-metadata';
+import { Post } from 'src/entity/Post';
+import { User } from 'src/entity/User';
+import { Comment } from 'src/entity/Comment';
+import config from 'ormconfig.json';
+
+const create = async () => {
+  // @ts-ignore
+  return createConnection({
+    ...config,
+    entities: [Post, User, Comment],
+  });
+};
 
 const promise = (async function () {
   const manager = getConnectionManager();
   const hasConnection = manager.has('default');
-  if (!hasConnection) return createConnection();
+  if (!hasConnection) return create();
   const current = manager.get('default');
   if (current.isConnected) return current;
-  return createConnection();
+  return create();
 })();
 
 export const getDatabaseConnection = async () => {
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,27 @@
 import { getDatabaseConnection } from 'lib/getDatabaseConnection';
 import { GetServerSideProps, NextPage } from 'next';
-import { createConnection, getConnection } from 'typeorm';
+import { Post } from 'src/entity/Post';
 import { UAParser } from 'ua-parser-js';
 import styles from '../styles/Home.module.css';
 
 interface Props {
+  posts: Post[];
   browser: {
     name: string;
     version: string;
     major: string;
   };
 }
+
 const Index: NextPage<Props> = (props) => {
+  const { posts } = props;
+
   return (
     <div>
       <h1 className={styles.container}>你的浏览器是 {props.browser.name}</h1>
+      {posts.map((p, index) => {
+        return <div key={index}>{p.title}</div>;
+      })}
     </div>
   );
 };
@@ -22,13 +29,15 @@ const Index: NextPage<Props> = (props) => {
 export default Index;
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
-  const connect = await getDatabaseConnection();
+  const connection = await getDatabaseConnection();
+  const posts = await connection.manager.find(Post);
 
   const ua = context.req.headers['user-agent'];
   const result = new UAParser(ua).getResult();
   return {
     props: {
       browser: result.browser,
+      posts: JSON.parse(JSON.stringify(posts)),
     },
   };
 };
