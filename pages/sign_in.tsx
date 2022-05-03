8f3a3350ca32714152909201d22a45f78b13b561
sean <770899447@qq.com>
2022-05-03 15:49:41 +0800

    消除一处ts-ignore

--- pages/sign_in.tsx
+++ pages/sign_in.tsx
@@ -1,6 +1,6 @@
 import axios, { AxiosResponse } from 'axios';
 import { withSession } from 'lib/withSession';
-import { GetServerSideProps, NextPage } from 'next';
+import { GetServerSideProps, GetServerSidePropsContext, NextPage } from 'next';
 import { useCallback, useState } from 'react';
 import { User } from 'src/entity/User';
 
@@ -83,13 +83,14 @@ const SignIn: NextPage<{ user: User }> = (props) => {
 
 export default SignIn;
 
-// @ts-ignore
-export const getServerSideProps: GetServerSideProps = withSession(async (context) => {
-  // @ts-ignore
-  const user = context.req.session.get('currentUser');
-  return {
-    props: {
-      user: JSON.parse(JSON.stringify(user)),
-    },
-  };
-});
+export const getServerSideProps: GetServerSideProps = withSession(
+  async (context: GetServerSidePropsContext) => {
+    // @ts-ignore
+    const user = context.req.session.get('currentUser');
+    return {
+      props: {
+        user: JSON.parse(JSON.stringify(user)),
+      },
+    };
+  }
+);
