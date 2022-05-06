35e2efb819fece644e5775f1df86912ef0f91784
sean <770899447@qq.com>
2022-05-06 15:27:43 +0800

    分页基本做出来

--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -2,31 +2,38 @@ import { getDatabaseConnection } from 'lib/getDatabaseConnection';
 import { GetServerSideProps, NextPage } from 'next';
 import Link from 'next/link';
 import { Post } from 'src/entity/Post';
-import { UAParser } from 'ua-parser-js';
 import styles from 'styles/Home.module.css';
+import qs from 'querystring';
 
 interface Props {
   posts: Post[];
-  browser: {
-    name: string;
-    version: string;
-    major: string;
-  };
+  count: number;
+  perPage: number;
+  page: number;
 }
 
 const PostsIndex: NextPage<Props> = (props) => {
-  const { posts } = props;
+  const { posts, count, page } = props;
 
   return (
     <div>
-      <h1 className={styles.container}>文章列表</h1>
+      <h1 className={styles.container}>文章列表（{count}）</h1>
       {posts.map((p) => (
-        <div key={p.id}>
+        <div key={p.id} className={styles.font}>
           <Link href={`/posts/${p.id}`}>
             <a>{p.title}</a>
           </Link>
         </div>
       ))}
+      <footer>
+        <Link href={`?page=${page - 1 || 1}`}>
+          <a> {'<'}</a>
+        </Link>
+        共 {count} 篇文章，当前是第 {page} 页
+        <Link href={`?page=${page + 1}`}>
+          <a> {'>'}</a>
+        </Link>
+      </footer>
     </div>
   );
 };
@@ -34,15 +41,22 @@ const PostsIndex: NextPage<Props> = (props) => {
 export default PostsIndex;
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
+  const query = context.req.url.split('?').pop();
+  const params = new URLSearchParams(query);
+  const page = parseInt(params.get('page'));
+  const perPage = 3;
   const connection = await getDatabaseConnection();
-  const posts = await connection.manager.find(Post);
+  const [posts, count] = await connection.manager.findAndCount(Post, {
+    skip: (page - 1) * perPage,
+    take: perPage,
+  });
+  console.log(posts);
 
-  const ua = context.req.headers['user-agent'];
-  const result = new UAParser(ua).getResult();
   return {
     props: {
-      browser: result.browser,
       posts: JSON.parse(JSON.stringify(posts)),
+      count,
+      page,
     },
   };
 };
