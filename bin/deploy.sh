fbd7b11fe15fff5b2c1d1c4df29cd136fcbdd728
sean <770899447@qq.com>
2022-05-11 23:11:10 +0800

    更新ssh

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -2,6 +2,10 @@ docker start db1 &&
 cd /home/blog/app/ &&
 git pull &&
 yarn install --production=false &&
+yarn build &&
+git apply migrate.patch &&
+yarn mr &&
+git reset --hard HEAD &&
 docker build . -t sean/node-web-app &&
 docker kill app &&
 docker rm app &&
