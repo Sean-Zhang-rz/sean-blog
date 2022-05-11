b44de60b0918075718be7fd7e263af3f6e10bd5a
sean <770899447@qq.com>
2022-05-11 22:47:01 +0800

    更新ssh

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,9 +1,9 @@
-docker start d60 &&
+docker start db1 &&
 cd /home/blog/app/ &&
 git pull &&
 yarn install --production=false &&
 docker build . -t sean/node-web-app &&
-# docker kill app &&
-# docker rm app &&
+docker kill app &&
+docker rm app &&
 docker run --name app -p 3000:3000 --network=host -d sean/node-web-app &&
 echo 'OK!'
